c394bf10c9cc424ad3331f9cb3636697e053c15c
ricksie <sonckie_fan@hotmail.com>
2014-03-11 14:41:24 +0100

    newest version

--- Camera_Related_Functions.cs
+++ Camera_Related_Functions.cs
@@ -8,11 +8,11 @@ using System.Linq;
 public class Camera_Related_Functions : MonoBehaviour
 {
 	
-		//values that are used for calculation
+		//values that are used for calculation.
 		private float heightWanted;
 		private float distanceWanted;
 	
-		// Exposed vars for the camera position from the target.
+		// Exposed variables for the camera position from the target.
 		private float height = 50f;
 		private float distance = 50f;
 	
@@ -23,22 +23,22 @@ public class Camera_Related_Functions : MonoBehaviour
 		private Quaternion lookAtRotation;
 		private Quaternion additionalRotation;
 		private Vector3 additionalOffset;
-		private Vector3 targetAdjustedPosition;
+		
+		//variables for vectors/quaternions.
 		private float r;
 		private float t;
-		private float newr;
-		private float newt;
-		private float a;
-		private float b;
-		private float speedMod = 10.0f;
-	
+		
+		// importing the variables from the public class for the animation scenario.
 		public int moveCamera (int CamNum, Transform target, bool doZoom, float zoomStep, float zoomSpeed, float min, float max, float time,
 		Transform _myTransform, bool Getswap, List<Transform> spheres, bool smooth, float damping, bool bewegen, float xspeed,
 		float yspeed, float factor, bool manualcamera, Vector3 point)
+
+		//update function for the animation.
 		{		
-				//if the option Zoom is selected
-				if (doZoom) {
-						// Record our mouse input.  If we zoom add this to our height and distance.
+				//zoom function.
+			if (doZoom){
+
+						// Record our mouse scrollwheel input.  If we zoom add this to our height and distance.
 						float mouseInput = Input.GetAxis ("Mouse ScrollWheel");
 						heightWanted -= zoomStep * mouseInput;
 						distanceWanted -= zoomStep * mouseInput;
@@ -47,17 +47,17 @@ public class Camera_Related_Functions : MonoBehaviour
 						heightWanted = Mathf.Clamp (heightWanted, min, max);
 						distanceWanted = Mathf.Clamp (distanceWanted, min, max);
  			
-						//defining again the values for height and distance
+						//smoothing the transformation between the current position and the desired position
 						height = Mathf.Lerp (height, heightWanted, time * zoomSpeed);
 						distance = Mathf.Lerp (distance, distanceWanted, time * zoomSpeed);
  
-						// Post our result.
+						// Post our result in the vector.
 						zoomResult = new Vector3 (0f, height, -distance);
-				}
+						}
 		
 		
-				//To make sure that target can be selected.
-				if (Getswap) {
+				//the function that allows to change the camera position to a different moving object
+			if (Getswap){
 						
 						// Obtains the position of the pedestrian you are currently following
 						Vector3 targetPosition = target.position;
@@ -66,8 +66,9 @@ public class Camera_Related_Functions : MonoBehaviour
 						startingOffset = startingOffset + zoomResult;
 						_myTransform.position = target.position + startingOffset;
 
-						if (Input.GetKey (KeyCode.A)) {
-								
+						//rotation around the object clockwise
+						if (Input.GetKey (KeyCode.A)) 
+								{
 								// Will create a new offset, using the current offset and multiplying this by a rotation around the focus
 								// of this offset, in this case the pedestrian. The camera position is then updated to hold this position.
 								additionalRotation = Quaternion.AngleAxis (1, Vector3.up);
@@ -84,10 +85,11 @@ public class Camera_Related_Functions : MonoBehaviour
 								
 								// The newly defined offset must be stored for the next time. 
 								startingOffset = additionalOffset;
-						}
-
-						if (Input.GetKey (KeyCode.D)) {
-				
+								}
+						
+						//rotation around the object counter clockwise
+						if (Input.GetKey (KeyCode.D)) 
+								{
 								// Will create a new offset, using the current offset and multiplying this by a rotation around the focus
 								// of this offset, in this case the pedestrian. The camera position is then updated to hold this position.
 								additionalRotation = Quaternion.AngleAxis (-1, Vector3.up);
@@ -104,119 +106,154 @@ public class Camera_Related_Functions : MonoBehaviour
 				
 								// The newly defined offset must be stored for the next time. 
 								startingOffset = additionalOffset;
+								}
 						}
-				}
 			
-// to be able to move the camera
-				if (manualcamera) {
-						if (Input.GetKey (KeyCode.DownArrow)) {
-								_myTransform.position -= _myTransform.forward * factor * xspeed;
-						}
-						if (Input.GetKey (KeyCode.UpArrow)) {
-								_myTransform.position += _myTransform.forward * factor * xspeed;
-						}
-						if (Input.GetKey (KeyCode.LeftArrow)) {
-								_myTransform.position -= _myTransform.right * factor * yspeed;
-						}
-						if (Input.GetKey (KeyCode.RightArrow)) {
-								_myTransform.position += _myTransform.right * factor * yspeed;
-						}
-						if (Input.GetKey (KeyCode.LeftControl)) {
-								_myTransform.position -= _myTransform.up * factor * yspeed;
-						}
-						if (Input.GetKey (KeyCode.LeftAlt)) {	
-								_myTransform.position += _myTransform.up * factor * yspeed;
-						}
-						
+		// the manual camera function
+				if (manualcamera) 
+						{
+								//movement function.
+								// if a button is pressed, the camera will be adjusted in the desired direction on the x,y and z axis
+								
+								//forward direction
+								if (Input.GetKey (KeyCode.DownArrow)) {
+										_myTransform.position -= _myTransform.forward * factor * xspeed;
+								}
+								//backwards direction
+								if (Input.GetKey (KeyCode.UpArrow)) {
+										_myTransform.position += _myTransform.forward * factor * xspeed;
+								}
+								//left direction
+								if (Input.GetKey (KeyCode.LeftArrow)) {
+										_myTransform.position -= _myTransform.right * factor * yspeed;
+								}
+								//right direction
+								if (Input.GetKey (KeyCode.RightArrow)) {
+										_myTransform.position += _myTransform.right * factor * yspeed;
+								}
+								//downwards direction
+								if (Input.GetKey (KeyCode.LeftControl)) {
+										_myTransform.position -= _myTransform.up * factor * yspeed;
+								}
+								//upwards direction
+								if (Input.GetKey (KeyCode.LeftAlt)) {	
+										_myTransform.position += _myTransform.up * factor * yspeed;
+								}
+								
+								//rotation function
+								// if a button is pressed, the camera will rotate around the x and y axis 
+								
+								//rotating clockwise
+								if (Input.GetKey (KeyCode.A)) {
+										r -= xspeed * factor;
+										_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+								}
+								//rotating counterclockwise
+								if (Input.GetKey (KeyCode.D)) {
+										r += xspeed * factor;
+										_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+								}
+								//rorating downwards
+								if (Input.GetKey (KeyCode.W)) {
+										t -= yspeed * factor;
+										t = Mathf.Clamp (t, -90, 35);
+										_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+								}
+								//rotating upwards
+								if (Input.GetKey (KeyCode.S)) {
+										t += yspeed * factor;
+										t = Mathf.Clamp (t, -90, 35);
+										_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+								}
 
-						if (Input.GetKey (KeyCode.A)) {
-								r -= xspeed * factor;
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
-						if (Input.GetKey (KeyCode.D)) {
-								r += xspeed * factor;
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
-						if (Input.GetKey (KeyCode.W)) {
-								t -= yspeed * factor;
-								t = Mathf.Clamp (t, -90, 35);
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
-						if (Input.GetKey (KeyCode.S)) {
-								t += yspeed * factor;
-								t = Mathf.Clamp (t, -90, 35);
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
 
-
-				}
-
+						}
+				//returning the number of the camera
 				return CamNum;
 		}
-
+		
+		// importing the variables from the public class for the geometry scenario.
 		public void geometryCamera (bool doZoom, float zoomStep, float zoomSpeed, float min, float max, float time, Transform _myTransform, bool smooth, float damping, bool bewegen, 
-	                           float xspeed, float yspeed, float factor, bool manualcamera, Vector3 point)
+	    	                            float xspeed, float yspeed, float factor, bool manualcamera, Vector3 point)
+		//update function for the geometry.
 		{
-				//if the option Zoom is selected
+				//zoom function.
 				if (doZoom) {
-						// Record our mouse input.  If we zoom add this to our height and distance.
-						float mouseInput = Input.GetAxis ("Mouse ScrollWheel");
-						heightWanted -= zoomStep * mouseInput;
-						distanceWanted -= zoomStep * mouseInput;
+							// Record our mouse input.  If we zoom add this to our height and distance.
+							float mouseInput = Input.GetAxis ("Mouse ScrollWheel");
+							heightWanted -= zoomStep * mouseInput;
+							distanceWanted -= zoomStep * mouseInput;
 			
-						// Make sure they meet our min/max values.
-						heightWanted = Mathf.Clamp (heightWanted, min, max);
-						distanceWanted = Mathf.Clamp (distanceWanted, min, max);
+							// Make sure they meet our min/max values.
+							heightWanted = Mathf.Clamp (heightWanted, min, max);
+							distanceWanted = Mathf.Clamp (distanceWanted, min, max);
 			
-						//defining again the values for height and distance
-						height = Mathf.Lerp (height, heightWanted, time * zoomSpeed);
-						distance = Mathf.Lerp (distance, distanceWanted, time * zoomSpeed);
+							//defining again the values for height and distance
+							height = Mathf.Lerp (height, heightWanted, time * zoomSpeed);
+							distance = Mathf.Lerp (distance, distanceWanted, time * zoomSpeed);
 			
-						// Post our result.
-						zoomResult = new Vector3 (0f, height, -distance);
-				}
+							// Post our result.
+							zoomResult = new Vector3 (0f, height, -distance);
+							}
 
-				// to be able to move the camera
-				if (manualcamera) {
-						if (Input.GetKey (KeyCode.DownArrow)) {
-								_myTransform.position -= _myTransform.forward * factor * xspeed;
-						}
-						if (Input.GetKey (KeyCode.UpArrow)) {
-								_myTransform.position += _myTransform.forward * factor * xspeed;
-						}
-						if (Input.GetKey (KeyCode.LeftArrow)) {
-								_myTransform.position -= _myTransform.right * factor * yspeed;
-						}
-						if (Input.GetKey (KeyCode.RightArrow)) {
-								_myTransform.position += _myTransform.right * factor * yspeed;
-						}
-						if (Input.GetKey (KeyCode.LeftControl)) {
-								_myTransform.position -= _myTransform.up * factor * yspeed;
-						}
-						if (Input.GetKey (KeyCode.LeftAlt)) {	
-								_myTransform.position += _myTransform.up * factor * yspeed;
-						}
+				// the manual camera function
+				if (manualcamera)
+						{
+							//movement function.
+							// if a button is pressed, the camera will be adjusted in the desired direction on the x,y and z axis
+							
+							//forward direction
+							if (Input.GetKey (KeyCode.DownArrow)){
+									_myTransform.position -= _myTransform.forward * factor * xspeed;
+							}
+							//backwards direction
+							if (Input.GetKey (KeyCode.UpArrow)) {
+									_myTransform.position += _myTransform.forward * factor * xspeed;
+							}
+							//left direction
+							if (Input.GetKey (KeyCode.LeftArrow)){
+									_myTransform.position -= _myTransform.right * factor * yspeed;
+							}
+							//right direction
+							if (Input.GetKey (KeyCode.RightArrow)) {
+									_myTransform.position += _myTransform.right * factor * yspeed;
+							}
+							//downwards direction
+							if (Input.GetKey (KeyCode.LeftControl)) {
+									_myTransform.position -= _myTransform.up * factor * yspeed;
+							}
+							//upwards direction
+							if (Input.GetKey (KeyCode.LeftAlt)) {	
+									_myTransform.position += _myTransform.up * factor * yspeed;
+							}
+							
+							//rotation function
+							// if a button is pressed, the camera will rotate around the x and y axis 
+
+							//rotating clockwise
+							if (Input.GetKey (KeyCode.A)) {
+									r -= xspeed * factor;
+									_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+							}
+							//rotating counter clockwise
+							if (Input.GetKey (KeyCode.D)) {
+									r += xspeed * factor;
+									_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+							}
+							//rotating downwards
+							if (Input.GetKey (KeyCode.W)) {
+									t -= yspeed * factor;
+									t = Mathf.Clamp (t, -90, 35);
+									_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+							}
+							//rotating upwards
+							if (Input.GetKey (KeyCode.S)) {
+									t += yspeed * factor;
+									t = Mathf.Clamp (t, -90, 35);
+									_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
+							}
 						
-						if (Input.GetKey (KeyCode.A)) {
-								r -= xspeed * factor;
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
 						}
-						if (Input.GetKey (KeyCode.D)) {
-								r += xspeed * factor;
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
-						if (Input.GetKey (KeyCode.W)) {
-								t -= yspeed * factor;
-								t = Mathf.Clamp (t, -90, 35);
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
-						if (Input.GetKey (KeyCode.S)) {
-								t += yspeed * factor;
-								t = Mathf.Clamp (t, -90, 35);
-								_myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, Quaternion.Euler (t, r, 0), 5);
-						}
-			
-				}
 
 		}
 }
